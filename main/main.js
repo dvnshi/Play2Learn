6c4172126d9ac23c4b4013dbe24226291e416c48
dvnshi <devanshijain460@gmail.com>
2024-06-27 00:11:15 +0530

    sound in flipcards

--- main/main.js
+++ main/main.js
@@ -193,3 +193,18 @@ chatbotToggler.addEventListener("click", () => document.body.classList.toggle("s
 
 // adding sounds to the navbar options 
       
+//   document.querySelectorAll('.sh-box').forEach(card => {
+//     card.addEventListener('mouseenter', () => {
+//       document.getElementById('hover-sound').play();
+//     });
+//   });
+
+  document.querySelectorAll('.flip-card').forEach(card => {
+    card.addEventListener('mouseenter', () => {
+      const sound = document.getElementById('hover-sound');
+      sound.currentTime = 0; // Rewind to start
+      sound.play();
+    });
+  });
+
+  
